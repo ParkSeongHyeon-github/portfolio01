26a180e3e30937297f76f4fd04ba8c1706d7a787
erroop <erroop0@gmail.com>
2026-03-18 17:12:47 +0900

    Fix Section01 for deployed JSON Server + safe fetch handling

--- src/components/main/Section01.tsx
+++ src/components/main/Section01.tsx
@@ -20,7 +20,7 @@ const Section01 = () => {
         const load = async() => {
             try{
                 const result = await Select(8);
-                setLeaseData(result.data);
+                setLeaseData(result);
             }catch{
                 alert('올바른 접근이 아닙니다.');
             }
@@ -50,16 +50,16 @@ const Section01 = () => {
                 return(
                     <SwiperSlide key={val.id}>
                         <div className="img">
-                            <img src={`/img/cars/${val.carname}_${val.carcolor[0].name}.png`} alt={val.carname} />
+                            <img src={`/img/cars/${val.carname}_${val.carcolor?.[0]?.name || 'default'}.png`} alt={val.carname} />
                         </div>
                         <div className="tit">
                             <p>{val.carbrand} {val.carname}</p>
                             <img src={`/img/brand/${val.carbrand}.jpg`} alt={val.carbrand} />
                         </div>
-                        <p className="car-model">차량 모델 : {val.carmodel[0].name}</p>
+                        <p className="car-model">차량 모델 : {val.carmodel?.[0]?.name || '-'}</p>
                         <ul className="car-price">
-                            <li className="price"><span>차량가격</span><span>{Number(val.carmodel[0].price).toLocaleString()}원</span></li>
-                            <li className="month-price"><span>월 렌탈료</span><span>{Math.ceil(Number(val.carmodel[0].price) / 48).toLocaleString()}원</span></li>
+                            <li className="price"><span>차량가격</span><span>{Number(val.carmodel?.[0]?.price || 0).toLocaleString()}원</span></li>
+                            <li className="month-price"><span>월 렌탈료</span><span>{Math.ceil(Number(val.carmodel?.[0]?.price || 0) / 48).toLocaleString()}원</span></li>
                         </ul>
                         <ul className="use-type">
                             <li>48개월</li>
